0d0740bdf83d3909962d2c73ad4fa3c120818191
wen <gawer521n52@gmail.com>
2023-11-24 11:17:53 +0800

    Initial Commit

---
+++ 10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10
+//
+//  Created by EN W on 2023/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
